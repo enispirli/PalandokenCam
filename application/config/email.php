a906f19a258dcc8d383054af93d609fe7c951599
ElifNur <en.ispirli@gmail.com>
2017-02-08 21:29:52 +0300

    E-mail işlemleri ayarları yapıldı

---
+++ application/config/email.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['protocol'] = 'smtp';  //mail protokolü
+$config['smtp_host'] = 'ssl://smtp.gmail.com';   //web sunucu bilgileri
+$config['smtp_user'] = 'grhn25@gmail.com';  //web mail adresi
+$config['smtp_pass'] = 'Dakka.4321'; //web mail şifresi
+$config['smtp_port'] = '465';   //web mail smtp portu
+$config['mailtype'] = 'html';
+
--- application/controllers/normal/Siparis.php
+++ application/controllers/normal/Siparis.php
@@ -28,46 +28,66 @@ class Siparis extends Base_Controller {
             'siparis_icerik' => $this->input->post('siparis_icerik'),
         );
         $email = $this->input->post('email');
+        
         if (empty($email)) {
             $this->session->set_flashdata("sonuc", "Email Bos Bırakılamaz");
-        } else {
+            redirect(base_url() . "normal/Siparis");
+            return;
+        }
+        
+       
 
-            $this->Database_Model->insert_data('siparis', $data);
+        $this->Database_Model->insert_data('siparis', $data);
 
-            $datasiparis = $this->Database_Model->getByColumn('siparis', 'email', $data['email'])[0];
+        $datasiparis = $this->Database_Model->getByColumn('siparis', 'email', $data['email'])[0];
 
-            //resmin config ayarları yapılıyor.
-            $config['upload_path'] = './uploads/'; // resmin nere yükleneceği
-            $config['allowed_types'] = 'jpg|png|jpeg'; // hangi dosya uzantıları kabul edilecek
-            $config['max_size'] = '400';
+        //resmin config ayarları yapılıyor.
+        $config['upload_path'] = './uploads/'; // resmin nere yükleneceği
+        $config['allowed_types'] = 'jpg|png|jpeg'; // hangi dosya uzantıları kabul edilecek
+        $config['max_size'] = '400';
 
-            //initialize upload class
-            $this->load->library('upload', $config);
+        //initialize upload class
+        $this->load->library('upload', $config);
 
 
-            for ($i = 0; $i < count($_FILES['usr_files']['name']); $i++) {
-                $_FILES['userfile']['name'] = $datasiparis->id . "_" . $_FILES['usr_files']['name'][$i];
-                $_FILES['userfile']['type'] = $_FILES['usr_files']['type'][$i];
-                $_FILES['userfile']['tmp_name'] = $_FILES['usr_files']['tmp_name'][$i];
-                $_FILES['userfile']['error'] = $_FILES['usr_files']['error'][$i];
-                $_FILES['userfile']['size'] = $_FILES['usr_files']['size'][$i];
+        for ($i = 0; $i < count($_FILES['usr_files']['name']); $i++) {
+            $_FILES['userfile']['name'] = $datasiparis->id . "_" . $_FILES['usr_files']['name'][$i];
+            $_FILES['userfile']['type'] = $_FILES['usr_files']['type'][$i];
+            $_FILES['userfile']['tmp_name'] = $_FILES['usr_files']['tmp_name'][$i];
+            $_FILES['userfile']['error'] = $_FILES['usr_files']['error'][$i];
+            $_FILES['userfile']['size'] = $_FILES['usr_files']['size'][$i];
 
-                $isUploaded = $this->upload->do_upload();
+            $isUploaded = $this->upload->do_upload();
 
-                if ($isUploaded) {
-                    //eğer resim upload klasörüne yüklenmisse resim tablosuna kayıt at
-                    $dataResim = array(
-                        'yol' => $_FILES['userfile']['name'],
-                        'siparis_id' => $datasiparis->id);
-                    $this->Database_Model->insert_data('siparisresim', $dataResim);
+            if ($isUploaded) {
+                //eğer resim upload klasörüne yüklenmisse resim tablosuna kayıt at
+                $dataResim = array(
+                    'yol' => $_FILES['userfile']['name'],
+                    'siparis_id' => $datasiparis->id);
+                $this->Database_Model->insert_data('siparisresim', $dataResim);
+
+                $this->load->library('email');
+                $this->email->initialize($config); //sunucu bilgilerini email kütüphanesine gönderdik
+
+                $isim = ($this->input->post('ad') . $this->input->post('soyad'));
+                $this->email->from("grhn25@gmail.com", "dasd"); //mail gönderen bilgileri
+                $this->email->to("en.ispirli@gmail.com"); //formdan gelen mail alıcı bilgileri
+                $this->email->subject("sda"); //Formdan gelen mail konusu
+                $this->email->message("sadsa"); //Formdan gelen mail içeriği
+                $send = $this->email->send();
+                if ($send) {
                     $this->session->set_flashdata("sonuc", "Siparişiniz başarıyla alınmıştır. En kısa zamanda dönüş yapılacaktır.");
-                    redirect(base_url() . "normal/Siparis");
-                } else {
-                    // hata aldı
-                    $this->session->set_flashdata("sonuc", $this->upload->display_errors());
                     redirect(base_url() . "normal/Siparis");
                     return;
                 }
+                
+                $this->session->set_flashdata("sonuc", $this->email->print_debugger());
+                redirect(base_url() . "normal/Siparis");
+            } else {
+                // hata aldı
+                $this->session->set_flashdata("sonuc", $this->upload->display_errors());
+                redirect(base_url() . "normal/Siparis");
+                return;
             }
         }
     }
